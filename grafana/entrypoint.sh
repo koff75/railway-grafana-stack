f06e8b94e045f84ef96f51e9e70b8c8e3285d259
koff75 <koff75@gmail.com>
2026-01-17 19:18:49 +0100

    Fix: chown provisioning pour user grafana (472) et sed portable; strip CRLF entrypoint

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-# Replace environment variables in datasources.yml
+# Replace environment variables in datasources.yml (temp file: portable, works as user grafana)
 if [ -f /etc/grafana/provisioning/datasources/datasources.yml ]; then
-  sed -i "s|\$LOKI_INTERNAL_URL|${LOKI_INTERNAL_URL:-http://loki.railway.internal:3100}|g" /etc/grafana/provisioning/datasources/datasources.yml
-  sed -i "s|\$PROMETHEUS_INTERNAL_URL|${PROMETHEUS_INTERNAL_URL:-http://prometheus.railway.internal:9090}|g" /etc/grafana/provisioning/datasources/datasources.yml
-  sed -i "s|\$TEMPO_INTERNAL_URL|${TEMPO_INTERNAL_URL:-http://tempo.railway.internal:3200}|g" /etc/grafana/provisioning/datasources/datasources.yml
+  sed "s|\$LOKI_INTERNAL_URL|${LOKI_INTERNAL_URL:-http://loki.railway.internal:3100}|g" /etc/grafana/provisioning/datasources/datasources.yml | \
+  sed "s|\$PROMETHEUS_INTERNAL_URL|${PROMETHEUS_INTERNAL_URL:-http://prometheus.railway.internal:9090}|g" | \
+  sed "s|\$TEMPO_INTERNAL_URL|${TEMPO_INTERNAL_URL:-http://tempo.railway.internal:3200}|g" > /tmp/datasources.yml && \
+  mv /tmp/datasources.yml /etc/grafana/provisioning/datasources/datasources.yml
 fi
 
 # Execute the original Grafana entrypoint
